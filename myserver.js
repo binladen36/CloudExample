28c0018df0bf64d828ceaa8d3cd05bcc4885bd41
binladen36 <59603151+binladen36@users.noreply.github.com>
2020-03-03 21:27:23 +0700

    test

---
+++ myserver.js
@@ -0,0 +1,30 @@
+var express=require('express');
+var app=express();
+app.set('view engine','jade');
+var port = process.env.PORT || 3000;
+
+//nhan anh o thu muc public
+var publicDir = require('path').join(__dirname,'/public');
+app.use(express.static(publicDir));
+
+//su dung saveForm
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/',function(req,res)
+{
+res.render('index');
+});
+
+app.get('/form',function(req,res)
+{
+res.render('form');
+});
+
+app.post('/saveForm',(req,res)=>
+{
+    //lay thong tin tu txtBox la yourName
+    const yourName= req.body.yourName;
+    res.render('confirm',{name:yourName})
+})
+var server=app.listen(port,function() {});
