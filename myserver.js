fa60c97781437856f0e2d96bea89cefb6ccec600
binladen36 <59603151+binladen36@users.noreply.github.com>
2020-03-05 22:52:05 +0700

    Update myserver.js

--- myserver.js
+++ myserver.js
@@ -1,30 +1,35 @@
+//use express module
 var express=require('express');
+//create an obj of the express module
 var app=express();
+//setting the view engine to Jade
 app.set('view engine','jade');
+//set the port 3000
 var port = process.env.PORT || 3000;
 
-//nhan anh o thu muc public
+//link path in public folder
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
-//su dung saveForm
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
+
+//create a callback function
 app.get('/',function(req,res)
 {
 res.render('index');
 });
 
+//create a login page function
 app.get('/form',function(req,res)
 {
 res.render('form');
 });
 
+//create a about page function
 app.get('/about',function(req,res)
 {
 res.render('about');
 });
 
-
+//make server lisen to the port
 var server=app.listen(port,function() {});
